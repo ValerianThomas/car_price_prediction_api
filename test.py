cabe2ae4556b1d22414565b1ae3f5f23558c327d
Valerian@OZ <valerian@ValerianOZs-MacBook-Pro.local>
2019-07-01 07:49:17 +0200

    removing api test as api not running

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from API.model.test.test_predict import test_make_prediction, test_api
 from API.model.regression_model.train_pipeline import run_training
 test_make_prediction()
-test_api()
+#test_api()
