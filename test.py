3a9e9452fdefa2f749dd0dfdf6e538fbe9b5f598
Valerian@OZ <valerian@ValerianOZs-MacBook-Pro.local>
2019-07-01 07:53:15 +0200

    double check

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from API.model.test.test_predict import test_make_prediction, test_api
+from API.model.test.test_predict import test_make_prediction
 from API.model.regression_model.train_pipeline import run_training
 test_make_prediction()
-#test_api()
